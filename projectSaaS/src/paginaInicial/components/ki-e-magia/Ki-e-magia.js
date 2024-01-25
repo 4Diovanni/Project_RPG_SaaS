d361cddd8aa4d764dd550f3f141f6e29edfe4d97
Giovanni Moreira <124792774+4Diovanni@users.noreply.github.com>
2024-01-25 15:11:18 -0300

    atualizando e redimensionando paginas
    adicionando css e estruturando a base com animações

--- projectSaaS/src/paginaInicial/components/ki-e-magia/Ki-e-magia.js
+++ projectSaaS/src/paginaInicial/components/ki-e-magia/Ki-e-magia.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Ki-e-magia.css'
+import Sun from './sun300x300.png'
+import Moon from './moon300x300.png'
 
-function KieMagia(){
-    return(
+function KieMagia() {
+    return (
         <div className="ki-e-magia">
-            <div className="ki-image"></div>
-            <div className="about-ki-e-magia"></div>
-            <div className="magia-image"></div>
-        </div>
+            <div className="ki-image"><Link to='/'><img className='sun-KM' src={Sun} alt='Sun SaaS' /></Link>
+            <Link to='/'><p>Ki</p></Link></div>
+            <div className="about-ki-e-magia">“Somos como Sol e Lua,
+                distanciados por corpo, mas
+                juntos pelo espirito”</div>
+            <div className="magia-image"><Link to='/paginaCombate'><img className='moon-KM' src={Moon} alt='Moon SaaS' /></Link>
+            <Link to='/'><p>Magia</p></Link></div>
+        </div >
     );
 }
 export default KieMagia;
