326c680f5ff68ebe938b6f07d1afb49b35a0e5aa
4Diovanni <diovanni.2566@gmail.com>
2024-02-03 18:24:55 -0300

    optmizando vitrine e tamanhos

--- projectSaaS/src/paginaInicial/components/ki-e-magia/Ki-e-magia.js
+++ projectSaaS/src/paginaInicial/components/ki-e-magia/Ki-e-magia.js
@@ -7,11 +7,11 @@ import Moon from './moon300x300.png'
 function KieMagia() {
     return (
         <div className="ki-e-magia">
-            <div className="ki-image"><Link to='/'><img className='sun-KM' src={Sun} alt='Sun SaaS' /></Link>
+            <div className="ki-image"><Link to='/paginaCombate'><img className='sun-KM' src={Sun} alt='Sun SaaS' /></Link>
             <Link to='/'><p>Ki</p></Link></div>
-            <div className="about-ki-e-magia">“Somos como Sol e Lua,
+            <div className="about-ki-e-magia"><p>“Somos como Sol e Lua,
                 distanciados por corpo, mas
-                juntos pelo espirito”</div>
+                juntos pelo espirito”</p></div>
             <div className="magia-image"><Link to='/paginaCombate'><img className='moon-KM' src={Moon} alt='Moon SaaS' /></Link>
             <Link to='/'><p>Magia</p></Link></div>
         </div >
--- projectSaaS/src/paginaInicial/components/vitrine/Vitrine.js
+++ projectSaaS/src/paginaInicial/components/vitrine/Vitrine.js
@@ -1,76 +1,70 @@
+// Carousel.js
 import React from 'react';
 import Slider from 'react-slick';
+import { Link } from 'react-router-dom';
+import Xiao from './xiao.png';
+import Armas from './armas.jpg';
+import Reliquias from './reliquias.jpg';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './Vitrine.css';
 
-function Vitrine() {
+// Declaração das funções antes de utilizá-las
+const PrevArrow = (props) => {
+  const { onClick } = props;
+  return <button className="arrow-button prev" onClick={onClick}></button>;
+};
+
+const NextArrow = (props) => {
+  const { onClick } = props;
+  return <button className="arrow-button next" onClick={onClick}></button>;
+};
+
+const Carousel = () => {
+  const data = [
+    { id: 1, name: 'Xiao', image: Xiao, link: '/xiao' },
+    { id: 2, name: 'Armas', image: Armas, link: '/armas' },
+    { id: 3, name: 'Relíquias', image: Reliquias, link: '/reliquias' },
+    { id: 1, name: 'Xiao', image: Xiao, link: '/xiao' },
+    { id: 2, name: 'Armas', image: Armas, link: '/armas' },
+    { id: 3, name: 'Relíquias', image: Reliquias, link: '/reliquias' },
+    // Adicione mais dados conforme necessário
+  ];
+
   const settings = {
-    dots: true,
     infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
     speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 2,
+    centerMode: true,
+    focusOnSelect: true,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
   };
 
   return (
-    <div className="layout-vitrine">
-       <div className='linha-vitrine'></div>
+    <div className='layout-carrossel-container'>
+      <div className='retangulo-center'>
+      <div className='linha-carrossel-topo'></div>
+      </div>
       <Slider {...settings}>
-        <div className='vitrine-layout'>
-          <div className="personagem">
-            <div className='titulo-personagem'>Personagem</div>
+        {data.map((item) => (
+          <div className='layout-carrosel' key={item.id}>
+            <div className='container-imagem'>
+              <img className='imagens-carrosel' src={item.image} alt={item.name} />
+              <Link to={item.link} style={{ textDecoration: 'none' }}>
+              <h3 className='imagens-nome'>{item.name}</h3>
+            </Link>
+            </div>
+            
           </div>
-        </div>
-
-        <div className='vitrine-layout'>
-          <div className="reliquias">
-            <div className='titulo-reliquias'>Reliquias</div>
-          </div>
-        </div>
-
-        <div className='vitrine-layout'>
-          <div className="armas">
-            <div className='titulo-armas'>Armas</div>
-          </div>
-        </div>
-        <div className='vitrine-layout'>
-          <div className="personagem">
-            <div className='titulo-personagem'>Personagem</div>
-          </div>
-        </div>
-
-        <div className='vitrine-layout'>
-          <div className="reliquias">
-            <div className='titulo-reliquias'>Reliquias</div>
-          </div>
-        </div>
-
-        <div className='vitrine-layout'>
-          <div className="armas">
-            <div className='titulo-armas'>Armas</div>
-          </div>
-        </div>
-        <div className='vitrine-layout'>
-          <div className="personagem">
-            <div className='titulo-personagem'>Personagem</div>
-          </div>
-        </div>
-
-        <div className='vitrine-layout'>
-          <div className="reliquias">
-            <div className='titulo-reliquias'>Reliquias</div>
-          </div>
-        </div>
-
-        <div className='vitrine-layout'>
-          <div className="armas">
-            <div className='titulo-armas'>Armas</div>
-          </div>
-        </div>
-        
+        ))}
       </Slider>
-      <div className='linha-vitrine'></div>
+      <div className='retangulo-center'>
+      <div className='linha-carrossel-baixo'></div>
+      </div>
     </div>
   );
-}
+};
 
-export default Vitrine;
+export default Carousel;
